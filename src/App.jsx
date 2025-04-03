96265bff5ffbcd75469676cbea5aaafdc506f745
Mike Trumbull <michael.trumbull@nscc.ca>
2025-04-03 12:17:06 -0300

    Added a form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  
+  function addContact(event) {
+    event.preventDefault();
+
+    // get the form data
+    const formData = new FormData(event.target);
+
+    // concert form data into JSON
+    const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+    // send the data to the server
+    const sendPost = async () => {
+      
+      const response = await fetch('https://nscc-0304263-inft400701-contacts-api-dxezaafhhmc3gdhd.canadacentral-01.azurewebsites.net/api/contacts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: json
+      })
+
+      const result = await response.json();
+
+      if (response.ok) {
+        alert('Ticket purchased successfully');
+      }
+      else {
+        alert('Error purchasing ticket' + result.message);
+      }
+
+    }
+
+    sendPost();
+    
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        Welcome to my ticket purchases
+      </h1>
+
+      <form onSubmit={addContact} method="post" className="w-50 mx-auto">
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Name</label>
+          <input type="text" className="form-control" id="name" name="name" placeholder="Enter your name" />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form> 
     </>
   )
 }
